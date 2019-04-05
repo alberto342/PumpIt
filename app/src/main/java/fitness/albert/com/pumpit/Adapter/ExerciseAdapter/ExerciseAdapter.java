cfee5c061aee1c84d026984b870a143a96454674
Albert <albert.brami@gmail.com>
2019-04-05 21:42:30 +0300

    Add realm file exercise, exercise adapter, exercise image

---
+++ app/src/main/java/fitness/albert/com/pumpit/Adapter/ExerciseAdapter/ExerciseAdapter.java
@@ -0,0 +1,109 @@
+package fitness.albert.com.pumpit.Adapter.ExerciseAdapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bumptech.glide.Glide;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import fitness.albert.com.pumpit.Model.Exercise;
+import fitness.albert.com.pumpit.R;
+import fitness.albert.com.pumpit.ShowExerciseItemActivity;
+
+
+public class ExerciseAdapter extends RecyclerView.Adapter<ExerciseViewHolder> implements SearchView.OnQueryTextListener {
+
+    private Context mContext;
+    private List<Exercise> exerciseList;
+    public static String exerciseImg;
+    public static String exerciseName;
+
+
+    public ExerciseAdapter(Context c, List<Exercise> exerciseList) {
+        this.mContext = c;
+        this.exerciseList = exerciseList;
+    }
+
+    @NonNull
+    @Override
+    public ExerciseViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(mContext).inflate(R.layout.item_exercise, parent, false);
+        return new ExerciseViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final ExerciseViewHolder holder, int position) {
+
+        final String TAG = "ExerciseAdapter";
+
+        Bitmap bitmap = getBitmapFromAsset(mContext, exerciseList.get(position).getImg_name());
+
+        Glide.with(mContext)
+                .load(bitmap)
+                .into(holder.image);
+
+        holder.nameTxt.setText(exerciseList.get(position).getName());
+
+        holder.layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+             int position =  holder.getAdapterPosition();
+                exerciseImg = exerciseList.get(position).getImg_name();
+                exerciseName = exerciseList.get(position).getName();
+                mContext.startActivity(new Intent(mContext, ShowExerciseItemActivity.class));
+                Log.d(TAG,"Exercise position: " + exerciseList.get(position).getName());
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return exerciseList.size();
+    }
+
+    public void updateList(List<Exercise> newList) {
+        exerciseList = new ArrayList<>();
+        exerciseList.addAll(newList);
+        notifyDataSetChanged();
+    }
+
+
+    @Override
+    public boolean onQueryTextSubmit(String s) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String s) {
+        return false;
+    }
+
+    private Bitmap getBitmapFromAsset(Context context, String filePath) {
+        AssetManager assetManager = context.getAssets();
+
+        InputStream istr;
+        Bitmap bitmap = null;
+        try {
+            istr = assetManager.open("images/" +filePath);
+            bitmap = BitmapFactory.decodeStream(istr);
+        } catch (IOException e) {
+            // handle exception
+        }
+        return bitmap;
+    }
+}
---
+++ app/src/main/java/fitness/albert/com/pumpit/ShowExerciseResult.java
@@ -0,0 +1,126 @@
+package fitness.albert.com.pumpit;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fitness.albert.com.pumpit.Adapter.ExerciseAdapter.ExerciseAdapter;
+import fitness.albert.com.pumpit.Model.Exercise;
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+import io.realm.RealmQuery;
+import io.realm.RealmResults;
+
+
+public class ShowExerciseResult extends AppCompatActivity implements SearchView.OnQueryTextListener {
+
+    private Realm realm;
+    RecyclerView recyclerView;
+    private List<Exercise> realmList = new ArrayList<>();
+    private ExerciseAdapter exerciseAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_exercise_result);
+
+        //SETUP REEALM
+        RealmConfiguration config = new RealmConfiguration.Builder().name("gym.realm").deleteRealmIfMigrationNeeded().build();
+        Realm.setDefaultConfiguration(config);
+
+        initRecyclerView();
+    }
+
+
+    private void initRecyclerView() {
+
+        //SETUP RECYCLERVIEW
+        recyclerView = findViewById(R.id.rv);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+
+        realm = Realm.getDefaultInstance();
+
+        recyclerView(this, realmList, AddExerciseActivity.categorySelected, AddExerciseActivity.category2Selected);
+    }
+
+
+    public void recyclerView(Context context, List<Exercise> exerciseList, String category, String category2) {
+
+        final String TAG = "ShowExerciseResult";
+        RealmQuery<Exercise> query = realm.where(Exercise.class);
+
+        if (category.contains("All")) {
+            query.findAll();
+        } else if (category2.contains("null")) {
+            query.equalTo("category", category);
+        } else {
+            query.equalTo("category_2", category2);
+        }
+
+        RealmResults<Exercise> result = query.findAll();
+
+        exerciseList.addAll(result);
+
+        exerciseAdapter = new ExerciseAdapter(context, exerciseList);
+        recyclerView.setAdapter(exerciseAdapter);
+
+        Log.d(TAG, "initRecyclerView: init recyclerView" + recyclerView + "\n" +
+                "IMAGE TEST: " + realmList.get(0).getImg_name());
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main_search_exercise, menu);
+        MenuItem menuItem = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setOnQueryTextListener(this);
+        return true;
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //realm.close();
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+
+        String userInput = newText.toLowerCase();
+        List<Exercise> newList = new ArrayList<>();
+
+        for (Exercise name : realmList) {
+            if (name.getName().toLowerCase().contains(userInput)) {
+                newList.add(name);
+            }
+        }
+        exerciseAdapter.updateList(newList);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        AddExerciseActivity.categorySelected = "null";
+        AddExerciseActivity.category2Selected = "null";
+        finish();
+    }
+
+
+}
